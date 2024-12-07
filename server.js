d700bbe7d61b008ce6736f6f1e056f6569488c15
Kimsungbin1 <ksb19558@naver.com>
2024-12-07 18:37:48 +0900

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(
   cors({
     origin: process.env.CORS_ORIGIN
       ? process.env.CORS_ORIGIN.split(",")
-      : ["http://127.0.0.1:5500", "http://localhost:5500"],
+      : ["https://wdj.kr", "https://wdjhs.netlify.app"],
     credentials: true,
   })
 );
